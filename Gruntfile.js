723279a2c9e0789e8c0ed0507549fc617fa8cf8c
Steve Barnett <steve@naga.co.za>
2015-08-21 11:18:43 +0200

    Add perfbudget with defaults

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,20 @@ module.exports = function(grunt) {
         jshintrc: true
       },
       files: 'Gruntfile.js'
+    },
+    perfbudget: {
+      default: {
+        options: {
+          url: 'http://example.com',
+          key: process.env.WPT_API_KEY
+        }
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-perfbudget');
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
