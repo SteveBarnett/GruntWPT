b75de36afb5e4ad8ed2d0eec450a21f5d9771910
Steve Barnett <steve@naga.co.za>
2015-08-21 11:07:08 +0200

    Add Grunt skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      files: 'Gruntfile.js'
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
